17fb45533dcc74370cbd20dd8fafa82c6c9d6e81
ItsHLa <nissanihala@gmail.com>
2026-08-19 10:43:57 +0300

    chore: initialize project structure and entry point

---
+++ main.py
@@ -0,0 +1,37 @@
+from src.agent import Agent
+from src.llm import LLMFactory
+# from src.memory import MemoryManager
+from src.vector_store import RAGVectorStore
+import asyncio
+from langgraph.checkpoint.memory import InMemorySaver
+from src.prompt import SYSTEM_PROMPT
+
+async def main():
+
+    model = LLMFactory.create(
+        provider = 'genai',
+        model = 'gemini-3.1-flash-lite-preview',
+        temperature = 0)
+    
+    # manager = MemoryManager()
+
+    # checkpointer = await manager.connect()
+
+    checkpointer = InMemorySaver()
+
+    vectorstore = RAGVectorStore()
+    tool = vectorstore.get_retriever_tool()
+    agent = Agent(
+        model = model,
+        checkpointer = checkpointer,
+        system_prompt = SYSTEM_PROMPT,
+        tools = [tool]
+    )
+
+    text = agent.call(user_prompt = 'hello, what are the main policy of the company? What data u collect?', session_id ="1")
+    print(text)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    
